c2cdc1ca546f274d4a368b435b59b0921d4010da
Ermina Joseph <ermina.joseph@gmail.com>
2019-11-01 22:30:10 -0400

    moving variable

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,5 @@
 var computerGuess = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var computerChoice = computerGuess[Math.floor(Math.random() * computerGuess.length)];
 
 var wins = 0;
 var winsText = document.getElementById("wins");
@@ -16,8 +17,6 @@ var numberOfGuessesText = document.getElementById("guesses-left");
 document.onkeyup = function(event) {
 
     var userChoice = event.key;
-
-    var computerChoice = computerGuess[Math.floor(Math.random() * computerGuess.length)];
     
     for (i = 0; i < computerGuess.length; i++){
         
@@ -29,7 +28,7 @@ document.onkeyup = function(event) {
                 losses++;
                 guessesSubmitted--;
             }
-            
+
             winsText.textContent = "Wins: " + wins;
             lossesText.textContent = "Losses: " + losses;
             numberOfGuessesText.textContent = "Guesses Left " + numberOfGuesses;
